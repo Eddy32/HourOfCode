d393c07f5650d87fa5c69b3e1bcb7c87e713ebcb
João Cabo <Ninjadinosaur.ilikecake@gmail.com>
2017-12-05 22:13:37 +0000

    FIcheiros para a HourOfCode2017

---
+++ pipe.js
@@ -0,0 +1,14 @@
+//desenha pipe superior
+/*
+function draw_Pipe_top(x,y,z){
+    var canvas = document.getElementById('defaultCanvas0');
+    var context = canvas.getContext('2d');
+    var pipe_main = new Image();
+    var pipe_mainAux = new Image();
+    z = z - 22;
+    pipe_main.src = 'pipe_main.png';
+    pipe_mainAux.src = 'pipe_aux.png';
+    context.drawImage(pipe_main,x+3,y,30,z);
+    context.drawImage(pipe_mainAux,x,z-3,36,22);
+}
+*/
