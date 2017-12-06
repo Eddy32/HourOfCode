0fcd588f832da1ba3e0347049e060d64154d55e7
Eddy32 <33222129+Eddy32@users.noreply.github.com>
2017-12-06 16:30:20 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,3 +8,11 @@ function setup() {
 function draw() {
 
 }
+
+function draw_background(){
+    var canvas = document.getElementById('defaultCanvas0');
+    var context = canvas.getContext('2d');
+    var back_image = new Image();
+    back_image.src = 'backflappy.png';
+    context.drawImage(back_image,0,0,400,600);
+}
