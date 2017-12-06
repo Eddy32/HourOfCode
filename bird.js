25d42a1e58bfb7f0fb2c291c7bb0e78d4e9c278e
Eddy32 <33222129+Eddy32@users.noreply.github.com>
2017-12-06 16:12:30 +0000

    Update bird.js

--- bird.js
+++ bird.js
@@ -1,1 +1,17 @@
 //NADA
+/*
+this.show = function() {
+    var canvas = document.getElementById('defaultCanvas0');
+    var context = canvas.getContext('2d');
+    draw_bird(context,this.x,this.y);
+
+  }
+  
+function draw_bird (s,x,y) {
+    var base_image = new Image();
+
+    base_image.src = 'bird2.png';
+    //alterei o tamanho
+    s.drawImage(base_image, x, y,30,26);
+}
+*/
